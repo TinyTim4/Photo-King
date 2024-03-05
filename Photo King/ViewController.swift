f47dbf6341302f98581c025cb0a196f04377aa80
TinyTim4 <tyl19@scarletmail.rutgers.edu>
2024-03-05 16:10:12 -0500

    Initial Commit

---
+++ Photo King/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Photo King
+//
+//  Created by Timothy Liu on 3/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var btnLogin: UIButton!
+    @IBOutlet var btnSignUp: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        btnLogin.layer.cornerRadius = btnLogin.frame.size.height/2
+        btnSignUp.layer.cornerRadius = btnSignUp.frame.size.height/2
+    }
+    
+    @IBAction func didTapLoginButton(sender: UIButton) {
+        print("Login Tapped")
+    }
+    @IBAction func didTapSignUpButton(sender: UIButton) {
+        print("Sign Up Tapped")    }
+
+
+}
+
